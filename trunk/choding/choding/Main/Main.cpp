23dbfa32f2a5d54640f61446f8464d4d071b9f90
ertai119@naver.com <ertai119@naver.com@76503172-4165-639c-619a-f0f1dd94456c>
2010-12-29 01:46:37 +0000

    솔루션 또 변경 ㅋㅋㅋ

--- trunk/choding/choding/Main/Main.cpp
+++ trunk/choding/choding/Main/Main.cpp
@@ -1,23 +1,20 @@
-//#include <Windows.h>
 #include <iostream>
 
-//#include "../../../chodingModule/Framework/Singleton/CSingleton.h"
-#include "Test.h"
 
 void CreateSingleton()
 {
-	Test::CreateSingleton();
+	//Test::CreateSingleton();
 }
 
 void DestroySingleton()
 {
-	Test::DestroySingleton();
+	//Test::DestroySingleton();
 }
 
 int main()
 {
 	CreateSingleton();
-	Test::GetInstance().Run();
+	//Test::GetInstance().Run();
 	std::cout << "hello world\n" ;
 
 	DestroySingleton();
