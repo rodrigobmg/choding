ce154a5a3970f1dd8aa173fa7356d17bb945fab0
ertai119@naver.com <ertai119@naver.com@76503172-4165-639c-619a-f0f1dd94456c>
2011-02-23 07:23:15 +0000

    우선 시작프로그램 두개로.-_-;;;

--- trunk/UnitTest/UnitTest++/UnitTest++.cpp
+++ trunk/UnitTest/UnitTest++/UnitTest++.cpp
@@ -2,10 +2,16 @@
 //
 
 #include "stdafx.h"
+#include "inc/UnitTest++.h"
+#pragma comment ( lib , "lib/UnitTest++.vsnet2005.lib" )
 
+TEST( hellounittest )
+{
+	CHECK( false );
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	return 0;
+	return UnitTest::RunAllTests();
 }
 
